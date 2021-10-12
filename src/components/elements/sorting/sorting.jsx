07afbc09550979341169756e0287d6645b426251
Alexey <awotjan@gmail.com>
2021-10-12 17:21:36 +0300

    поправит сортировку с клавиатуры

--- src/components/elements/sorting/sorting.jsx
+++ src/components/elements/sorting/sorting.jsx
@@ -13,6 +13,18 @@ function Sorting() {
     dispatch(changeDirection(''));
   };
 
+  const handleKeyDownSortClick = (sortType, e) => {
+    if (e.key === 'Enter') {
+      exchangeSort(sortType);
+    }
+  };
+
+  const handleKeyDownDiretionClick = (direction, e) => {
+    if (e.key === 'Enter') {
+      dispatch(changeDirection(direction));
+    }
+  };
+
   const exchangeDirection = (sortDirection) => dispatch(changeDirection(sortDirection));
 
   return (
@@ -22,7 +34,13 @@ function Sorting() {
       <ul className={styles.list}>
         {
           Object.entries(SortOptions).map(([name, option]) => (
-            <li key={name} className={styles.item} onClick={() => exchangeSort(name.toLowerCase())}>
+            <li
+              key={name}
+              className={styles.item}
+              onClick={() => exchangeSort(name.toLowerCase())}
+              onKeyDown={(e) => handleKeyDownSortClick(name.toLowerCase(), e)}
+              tabIndex='0'
+            >
               {option}
             </li>
           ))
@@ -31,10 +49,14 @@ function Sorting() {
       <ul className={styles.additional_list}>
         <li className={styles.addition_item}
           onClick={() => exchangeDirection('вверх')}
+          onKeyDown={(e) => handleKeyDownDiretionClick('вверх', e)}
+          tabIndex='0'
         >
         </li>
         <li className={cn(styles.addition_item, styles.addition_item__reverse)}
           onClick={() => exchangeDirection('вниз')}
+          onKeyDown={(e) => handleKeyDownDiretionClick('вниз', e)}
+          tabIndex='0'
         >
         </li>
       </ul>
